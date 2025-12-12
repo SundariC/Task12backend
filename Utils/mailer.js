92b8d143a96f8277139ae9e877092d0dd9a82b12
Sundari <csundari2000@gmail.com>
2025-12-13 00:28:10 +0530

    user added

---
+++ Utils/mailer.js
@@ -0,0 +1,29 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.PASS_MAIL,
+    pass: process.env.PASS_KEY,
+  },
+});
+
+const sendEmail = async (to, subject, text) => {
+  const mailOptions = {
+    from: process.env.PASS_MAIL,
+    to,
+    subject,
+    text,
+  };
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Email send successfully");
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+};
+
+export default sendEmail;
---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./Database/dbConfig.js";
+import authRoute from "./Routes/authRoute.js";
+
+dotenv.config();
+
+const app= express();
+
+app.use(express.json());
+app.use(cors());
+
+connectDB();
+
+app.get("/",(req,res) => {
+    res.send("welcome to backed")
+})
+
+app.use("/api/auth", authRoute);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=>  {
+    console.log("server started");
+})
