f560a972a18fd75577786709a27a8a59163e0e45
Sundari <csundari2000@gmail.com>
2025-12-26 22:27:17 +0530

    added

--- index.js
+++ index.js
@@ -6,22 +6,28 @@ import authRoute from "./Routes/authRoute.js";
 
 dotenv.config();
 
-const app= express();
+const app = express();
 
 app.use(express.json());
-app.use(cors({
-origin:"https://task12frontend.netlify.app"}));
+
+app.use(
+  cors({
+    origin: "https://task12frontend.netlify.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 connectDB();
 
-app.get("/",(req,res) => {
-    res.send("welcome to backed")
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to Backend 🚀");
+});
 
 app.use("/api/auth", authRoute);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=>  {
-    console.log("server started");
-})
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
